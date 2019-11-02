dbb200429fdbd32364ed1f51a3d6c5e52bf710da
Chris Chou <chhschou@gmail.com>
2019-11-02 22:56:10 +1300

    update

--- .zsh/zplugin/cmdline_prog.zsh
+++ .zsh/zplugin/cmdline_prog.zsh
@@ -30,7 +30,23 @@ zplugin light gpakosz/.tmux
 zplugin ice lucid nocompile
 zplugin light tmux-plugins/tpm
 
-# docker-compose
+# docker
+zplugin ice lucid id-as'docker-install' as"program" \
+  mv'docker-install* -> docker.tgz' \
+  atclone'tar xzvf *.tgz; rm *.tgz;' \
+  atpull'%atclone' \
+  pick'docker/containerd' sbin'docker/dockerd' sbin'docker/docker'
+zplugin snippet "https://download.docker.com/linux/static/stable/x86_64/docker-19.03.4.tgz"
+
+# # dockerd-rootless.sh --experimental
+# zplugin ice lucid wait'1' id-as'dockerd-rootless' as"program" \
+#   mv'dockerd-rootless* -> dockerd-rootless.tgz' \
+#   atclone'tar xzvf *.tgz; rm *.tgz;' \
+#   atpull'%atclone' \
+#   sbin'docker-rootless-extras/rootlesskit' sbin'docker-rootless-extras/vpnkit' \
+#   atload'docker-rootless-extras/dockerd-rootless.sh --experimental'
+# zplugin snippet "https://download.docker.com/linux/static/stable/x86_64/docker-rootless-extras-19.03.4.tgz"
+
 zplugin ice from"gh-r" as"program" mv"docker* -> docker-compose"
 zplugin light docker/compose
 
