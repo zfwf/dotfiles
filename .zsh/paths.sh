df8a5e82c7e54fa4251315027479b9a639036bb7
Chris Chou <chhschou@gmail.com>
2019-12-06 22:41:31 +1300

    update

--- .zsh/paths.sh
+++ .zsh/paths.sh
@@ -1,9 +1,18 @@
 # android
-if [ -d "$HOME/Android/Sdk" ]; then
-  export ANDROID_HOME=$HOME/Android/Sdk
-  if [ -d "$ANDROID_HOME/ndk_bundle" ]; then
-    export NDK_ROOT=$ANDROID_HOME/ndk_bundle
-    export ANDROID_NDK_HOME=$NDK_ROOT
+case `uname` in
+  Darwin)
+    sdk_root=$HOME/Library/Android/sdk
+    ;;
+  Linux)
+    sdk_root=$HOME/Android/sdk
+    ;;
+esac
+
+if [ -d "$sdk_root" ]; then
+  export ANDROID_SDK_ROOT=$sdk_root
+  ndk_root="$ANDROID_SDK_ROOT/ndk_bundle"
+  if [ -d "$ndk_root" ]; then
+    export ANDROID_NDK_HOME=$ndk_root
   fi
-  PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin
+  PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin
 fi
