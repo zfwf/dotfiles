c76a4528e0c81afa119ea076332170a66033d713
Chris Chou <chhschou@gmail.com>
2020-02-08 16:33:44 +1300

    promote firefox to common gui prog

--- .zsh/zplugin/gui_prog_mac.zsh
+++ .zsh/zplugin/gui_prog_mac.zsh
@@ -1,42 +1,3 @@
-# copy dmg content to current directory
-extract_dmg() {
-  local dmg_name="*.dmg"
-  local content="/Volumes/**/*.app"
-  local dmg_volume=$1
-
-  local attached_vol=$(eval "hdiutil attach $(realpath -m $dmg_name)" | tail -n1 | cut -f 3)
-  eval "cp -r $(realpath -m $content) ."
-  echo "att vol $(realpath -m $attached_vol)"
-  eval "hdiutil detach \"$attached_vol\""
-}
-
-# create alias for spotlight to find apps
-remove_alias_in_user_applications() {
-  local app_path=$(realpath -m $1)
-  local app_name=${$(basename $app_path)%.*}
-  local user_applications_path=$(realpath -m ~/Applications)
-
-  eval "rm -f '$user_applications_path/$app_name'*"; # remove old alias
-}
-
-
-# create alias for spotlight to find apps
-create_alias_in_user_applications() {
-  local app_path=$(realpath -m $1)
-  local app_name=${$(basename $app_path)%.*}
-  local user_applications_path=$(realpath -m ~/Applications)
-  local script="tell application \"Finder\" to make alias file to \
-    POSIX file \"$app_path\" at \
-    POSIX file \"$user_applications_path\""
-
-  osascript -e "$script"
-}
-
-install_dotapp() {
-  remove_alias_in_user_applications *.app
-  create_alias_in_user_applications *.app
-}
-
 
 # vscode
 zinit ice lucid from"gh-r" bpick"*darwin*.zip" \
@@ -75,10 +36,8 @@ zinit ice lucid id-as'azure-data-studio' \
   as'null' sbin'**/Electron -> ads'
 zinit snippet "https://go.microsoft.com/fwlink/?linkid=2109180"
 
-# ff dev edition
-zinit pack"bgn" atclone+'install_dotapp' for firefox-dev
 
-#meld
+# meld
 zinit ice lucid from"gh-r" \
   atclone'install_dotapp' \
   atpull'%atclone' \
