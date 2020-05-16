e4a6f67f59f4cd65d06606ed84171c11e4596255
Chris Chou <chhschou@gmail.com>
2020-05-16 13:14:09 +1200

    add font.zsh

---
+++ .zsh/zplugin/font.zsh
@@ -0,0 +1,16 @@
+
+_get_user_font_dir() {
+  case `uname` in
+    Darwin)
+      echo $HOME/Library/Fonts
+      ;;
+    Linux)
+      echo $HOME/.local/share/fonts
+      ;;
+  esac
+}
+
+zinit ice from'gh-r' bpick'FiraCode.zip' \
+  atclone'zinit_font_dir=$(_get_user_font_dir); mkdir -p $zinit_font_dir; ln -sf $PWD $zinit_font_dir/FiraCode;' \
+  atpull'%atclone'
+zinit light ryanoasis/nerd-fonts
