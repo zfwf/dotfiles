a1413238ee2c31b57be46363946287110ae4388b
Chris Chou <chhschou@gmail.com>
2020-07-25 00:11:47 +1200

    refactor: remove unused completion

--- .zinit/init.d/comp.zsh
+++ .zinit/init.d/comp.zsh
@@ -10,8 +10,4 @@ zinit light zsh-users/zsh-autosuggestions
 
 #  completions
 zinit ice wait'1' lucid atload"unalias grv g"
-zinit snippet OMZ::plugins/git/git.plugin.zsh
-
-zinit ice wait'1' blockf lucid
-zinit light zsh-users/zsh-completions
-
+zinit snippet OMZP::git
