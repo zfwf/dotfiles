6d09221337417e1c1fe72cc205d44e24d87844c2
Chris Chou <chhschou@hotmail.com>
2023-01-08 15:49:24 +1300

    scripted os deps

--- .pre-init.sh
+++ .pre-init.sh
@@ -1,73 +1,74 @@
 # source from $HOME/.zshenv for zsh and $HOME/.profile for bash
 
-case `uname` in
-  Darwin)
-    # brew path
-    PATH="/usr/local/bin":$PATH
-    if [ ! -f $HOME/.gitconfig ]; then
-
-      # create symlinks
-      ln -sf $HOME/.gitconfig_mac $HOME/.gitconfig
-      ln -sf $HOME/.config/alacritty/alacritty_mac.yml $HOME/.config/alacritty/alacritty.yml
-      ln -sf $HOME/Brewfile_mac $HOME/Brewfile
-
-    fi
-
-    # install brew if not found
-    if [[ $(command -v brew) == "" && ! -d /opt/homebrew ]] ; then
-      # Install Homebrew
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-      brew bundle --no-lock
-    fi
-
-    ;;
-  Linux)
-    if [ ! -f $HOME/.gitconfig ]; then
-      # create symlinks
-      ln -sf $HOME/.gitconfig_linux $HOME/.gitconfig
-      ln -sf $HOME/.config/alacritty/alacritty_linux.yml $HOME/.config/alacritty/alacritty.yml
-
-      eval "OS_$(cat /etc/*-release | grep "^ID=")"
-      case "$OS_ID" in
-        "clear-linux-os")
-          ;;
-        "manjaro")
-          ;;
-      esac
-
-      # show desktop icons in gnome
-      if [[ $(command -v gsettings) ]] ; then
-        gsettings set org.gnome.desktop.background show-desktop-icons true
+if [ ! -f $HOME/.os-init ]; then
+  case `uname` in
+    Darwin)
+      if [ ! -f $HOME/.gitconfig ]; then
+        # symlinks
+        ln -sf $HOME/.gitconfig_mac $HOME/.gitconfig
+        ln -sf $HOME/Brewfile_mac $HOME/Brewfile
       fi
-    fi
-    ;;
-esac
+
+      # brew
+      if [[ $(command -v brew) == "" && ! -d /opt/homebrew ]] ; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        brew bundle --no-lock
+      fi
+
+      # add brew path
+      PATH="/usr/local/bin":$PATH
+      ;;
+    Linux)
+      if [ ! -f $HOME/.gitconfig ]; then
+        # create symlinks
+        ln -sf $HOME/.gitconfig_linux $HOME/.gitconfig
+
+        eval "OS_$(cat /etc/*-release | grep "^ID=")"
+        case "$OS_ID" in
+          "clear-linux-os")
+            sudo swupd bundle-add devpkg-bzip2 devpkg-openssl devpkg-readline devpkg-sqlite-autoconf devpkg-libffi x11-tools package-utils
+            ;;
+          "manjaro")
+            # xclip for clipboard support
+            sudo pacman -S base-devel xclip
+            ;;
+          "ubuntu")
+            ;& # fall through
+          "debian")
+            sudo apt install libssl-dev build-essential pkg-config
+            ;;
+        esac
+
+        # show desktop icons in gnome
+        if [[ $(command -v gsettings) ]] ; then
+          gsettings set org.gnome.desktop.background show-desktop-icons true
+        fi
+      fi
+      ;;
+  esac
+
+  # os init complete
+  touch $HOME/.os-init
+fi
 
 
-
-# install asdf if not found
+# asdf
 if [ ! -d $HOME/.asdf ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
   . ~/.asdf/asdf.sh;
   asdf plugin-add nodejs;
+  asdf plugin-add python;
   asdf plugin-add rust;
-  asdf plugin-add sbt;
-  export NODEJS_CHECK_SIGNATURES=no;
   cd $HOME; asdf install; asdf reshim;
-  asdf global nodejs $(asdf list nodejs);
-  asdf global rust $(asdf list rust);
-  asdf global sbt $(asdf list sbt);
 else
   . ~/.asdf/asdf.sh;
-  # completions handled by zinit
-  export NODEJS_CHECK_SIGNATURES=no;
   # integrate with java
   [ -f "$HOME/.asdf/plugins/java/set-java-home.zsh" ] && . ~/.asdf/plugins/java/set-java-home.zsh
-
-  if [[ $(command -v sccache) != "" ]] ; then
-    # enable sccache
-    export RUSTC_WRAPPER="$(command -v sccache)"
-  fi
-
 fi
+
+if [[ $(command -v sccache) != "" ]] ; then
+  # enable sccache
+  export RUSTC_WRAPPER="$(command -v sccache)"
+fi
+
 
